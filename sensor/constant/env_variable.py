eac610e2c52f554a912127bcd618a5c18ecc675e
Oladapoduk <97230768+Oladapoduk@users.noreply.github.com>
2023-04-07 21:09:47 +0100

    test_stage4

---
+++ sensor/constant/env_variable.py
@@ -0,0 +1,6 @@
+# MONGODB_URL_KEY = "MONGO_DB_URL"
+MONGODB_URL_KEY = "mongodb+srv://oladapo:Lotkiknef2341@cluster0.il8mksy.mongodb.net/?retryWrites=true&w=majority"
+
+AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+REGION_NAME = "us-east-1"
